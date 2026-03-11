263fa91fed9ae6b779bff84d4b57628d6d176c48
Karlo Buhinjak <buhinjak.k@gmail.com>
2026-03-11 23:09:51 +0100

    built static files

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
 import express from "express";
-import fetch from "node-fetch";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 
+const ENDPOINT =
+  "http://0dd429b2-2d8d-41da-ae59-f7534e7fafd4.francecentral.azurecontainer.io/score";
+
 app.post("/predict", async (req, res) => {
   try {
-    const response = await fetch(
-      "http://0dd429b2-2d8d-41da-ae59-f7534e7fafd4.francecentral.azurecontainer.io/score",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(req.body),
+    const response = await fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-    );
+      body: JSON.stringify(req.body),
+    });
 
     const data = await response.json();
     res.json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Greška pri pozivu Azure ML endpointa" });
+    res.status(500).send("Prediction error");
   }
 });
 
-const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`Proxy server running on http://localhost:${PORT}`),
-);
+/* SERVE REACT */
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
